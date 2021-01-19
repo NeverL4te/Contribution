dea7e9ac8e3588fcc39a6bbedaf3afb2b4d20c25
TranCongVinh <trancongvinh08@gmail.com>
2021-01-19 08:51:12 +0000

    Initial Commit

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ while (count < 99999):
         d = str(i) + ' day ago'
         with open('text.txt', 'a') as file:
             file.write(d)
-        
+    
         os.system('rm .git/gc.log')
         os.system('git commit --date="' + d + '" -m "Initial Commit"')
     os.system('git push -u origin main')
