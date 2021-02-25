fc6f47de1590621d12d7c12fa4baa19125e45867
TranCongVinh <trancongvinh08@gmail.com>
2021-02-25 08:47:37 +0000

    Initial Commit

--- main.py
+++ main.py
@@ -14,5 +14,5 @@ while (count < 99999):
         os.system('git commit --date="' + d + '" -m "Initial Commit"')
     os.system('git push -u origin main')
     os.system('clear')
-    os
+    os.syste
     time.sleep(20)
