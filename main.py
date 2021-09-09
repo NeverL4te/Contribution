4285d0192496f1cb4500e1012898ec1513c3088f
TranCongVinh <trancongvinh08@gmail.com>
2021-09-09 15:23:18 +0700

    Initial Commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+
+for i in range(1,365*9 + 1):
+    d = str(i) + ' day ago'
+    with open('text.txt', 'a') as file:
+        file.write(d)
+    os.system('git add .')
+    os.system('git commit --date="' + d + '" -m "Initial Commit"')
+
+os.system('git push -u origin main')
