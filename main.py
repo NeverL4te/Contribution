e1b0392037b7f3a0a1bb4656cf945fc7099937c9
TranCongVinh <trancongvinh08@gmail.com>
2021-09-23 18:32:29 +0700

    Initial Commit

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ while (count < 99999):
                 os.system('git commit --date="' + d + '" -m "Initial Commit"')
             os.system('git push -u origin main')
             os.system('clear')
+            time.sleep(15)
         os.system('git gc --prune=now')
         os.system('rm text.txt')
         ox.system('touch text.txt')
-        time.sleep(15)
