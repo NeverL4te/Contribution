2c18b23121795a5d5ed86707f3df9f3f6228f10e
TranCongVinh <trancongvinh08@gmail.com>
2021-09-23 18:29:29 +0700

    Initial Commit

--- main.py
+++ main.py
@@ -9,12 +9,18 @@ while (count < 99999):
     count = 0
     while (count < 99999):
         count = count + 1
-        for i in range(1,365*1 + 1):
-            d = str(i) + ' day ago'
-            with open('text.txt', 'a') as file:
-                file.write(d)
-            os.system('git add .')
-            os.system('git commit --date="' + d + '" -m "Initial Commit"')
-        os.system('git push -u origin main')
-        os.system('clear')
+        count = 0
+        while (count < 50):
+            count = count + 1
+            for i in range(1,365*1 + 1):
+                d = str(i) + ' day ago'
+                with open('text.txt', 'a') as file:
+                    file.write(d)
+                os.system('git add .')
+                os.system('git commit --date="' + d + '" -m "Initial Commit"')
+            os.system('git push -u origin main')
+            os.system('clear')
+        os.system('git gc --prune=now')
+        os.system('rm text.txt')
+        ox.system('touch text.txt')
         time.sleep(15)
