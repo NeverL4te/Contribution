cbee35355becdbddef5b091112b8811cd7dc785a
TCV <90259221+TranCongVinh@users.noreply.github.com>
2021-09-26 17:18:22 +0700

    Initial Commit

--- main.py
+++ main.py
@@ -4,23 +4,17 @@ from keep_alive import keep_alive
 keep_alive()
 
 count = 0
-while (count < 99999):
+while (count < 100):
     count = count + 1
-    count = 0
-    while (count < 99999):
-        count = count + 1
-        count = 0
-        while (count < 50):
-            count = count + 1
-            for i in range(1,365*1 + 1):
-                d = str(i) + ' day ago'
-                with open('text.txt', 'a') as file:
-                    file.write(d)
-                os.system('git add .')
-                os.system('git commit --date="' + d + '" -m "Initial Commit"')
-            os.system('git push -u origin main')
-            os.system('clear')
-            time.sleep(15)
-        os.system('git gc --prune=now')
-        os.system('rm text.txt')
-        os.system('touch text.txt')
+    for i in range(1,365*1 + 1):
+        d = str(i) + ' day ago'
+        with open('text.txt', 'a') as file:
+            file.write(d)
+        os.system('git add .')
+        os.system('git commit --date="' + d + '" -m "Initial Commit"')
+    os.system('git push -u origin main')
+    os.system('clear')
+    time.sleep(15)
+os.system('git gc --prune=now')
+os.system('rm text.txt')
+os.system('touch text.txt')
