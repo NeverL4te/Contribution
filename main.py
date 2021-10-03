df9cb7ede5b1c28cbf3ee1673a811a683f4c77e1
TranCongVinh <trancongvinh08@gmail.com>
2021-10-03 12:44:31 +0000

    Initial    Commit

--- main.py
+++ main.py
@@ -4,18 +4,21 @@ import time
 count = 0
 while (count < 99999):
     count = count + 1
-    for i in range(5):
-        for j in range(1,365*1 + 1):
-            d = str(i) + ' day ago'
-            with open('text.txt', 'a') as file:
-                file.write(d)
-            os.system('git add .')
-            os.system('git commit --date="' + d + '" -m "Initial Commit"')
-        os.system('git push -u origin main')
-        os.system('rm text.txt')
-        os.system('touch text.txt')
+    for k in range(200):
+        for i in range(5):
+            for j in range(1,365*1 + 1):
+                d = str(i) + ' day ago'
+                with open('text.txt', 'a') as file:
+                    file.write(d)
+                os.system('git add .')
+                os.system('git commit --date="' + d + '" -m "Initial    Commit"')
+            os.system('git push -u origin main')
+            os.system('rm text.txt')
+            os.system('touch text.txt')
+            os.system('clear')
+            time.sleep(30)
+        os.system('git gc; git repack -Ad')
         os.system('clear')
         time.sleep(30)
-    os.system('git fsck; git gc --prune=now; git repack -Ad')
-    os.system('clear')
+    os.system('git fsck; git gc --prune=now')
     time.sleep(30)
