85a8263cd3bf687e1d2551ec3938ffc9e1dc18e4
TranCongVinh <trancongvinh08@gmail.com>
2021-10-04 03:06:34 +0000

    Initial Commit

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ while (count < 99999):
                 with open('text.txt', 'a') as file:
                     file.write(d)
                 os.system('git add .')
-                os.system('git commit --date="' + d + '" -m "Initial    Commit"')
+                os.system('git commit --date="' + d + '" -m "Initial Commit"')
             os.system('git push -u origin main')
             os.system('rm text.txt')
             os.system('touch text.txt')
