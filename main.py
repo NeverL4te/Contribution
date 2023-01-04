5dca061a13078e4b2440edb409493d762c9ca574
Captain Throwback <trancongvinh08@gmail.com>
2023-01-04 20:54:30 +0700

    u

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ x = int(input("How many times do you want to commit? \n"))
 for i in range(x):
 	os.system(f'git commit --allow-empty -m "Commit {i} of {x}"')
 print("Commited " + str(x) + " times")
-	os.system('git push')
+os.system('git push')
