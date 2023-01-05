b958020043d2e9011955315453fc5f0735505170
Captain Throwback <trancongvinh08@gmail.com>
2023-01-05 21:04:09 +0700

    Initial Commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+#!/usr/bin/env python
 import os
 x = int(input("How many times do you want to commit? \n"))
 for i in range(x):
-	os.system(f'git commit --allow-empty -m "Commit {i} of {x}"')
-print("Commited " + str(x) + " times")
-os.system('git push')
+	os.system(f'git commit --allow-empty -m "Initial Commit"')
+	print(f"Commited {i} of {x}")
+os.system('git push;clear')
